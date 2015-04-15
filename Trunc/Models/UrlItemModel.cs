d5dd4c9b61fcd49211315d7f477904ce9219c084
mjsumerano <mjsumerano@localhost>
2015-04-14 22:59:38 -0400

    Added validation to allow only letters, numbers, - and _ for short URLs when manually entered

--- Trunc/Models/UrlItemModel.cs
+++ Trunc/Models/UrlItemModel.cs
@@ -4,6 +4,7 @@ using Trunc.Data;
 namespace Trunc.Models {
     public class UrlItemModel {
         [MinLength(3, ErrorMessage = "Must be 3 or more characters")]
+        [RegularExpression(@"([a-zA-Z0-9_\-]+)", ErrorMessage="Must use only letters, numbers, - or _")]
         [Display(Name = "Shorten URL")]
         public string ShortenUrl { get; set; }
 
