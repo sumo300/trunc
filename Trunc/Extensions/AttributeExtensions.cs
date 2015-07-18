41d4b29468be00cd2bb673d1103bdbe68cab4b9a
Sumerano, Michael J <mjsumerano@thehealthplan.com>
2015-07-18 00:44:42 -0400

    Fixed minor issue with getting the localized value for the ExpireMode enum

---
+++ Trunc/Extensions/AttributeExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+using System.Resources;
+
+namespace Trunc.Extensions {
+    public static class AttributeExtensions {
+        private static readonly ResourceManager _resource;
+
+        public static string GetDisplayValue(this Enum enumValue) {
+            FieldInfo fi = enumValue.GetType().GetField(enumValue.ToString());
+
+            var attributes = (DisplayAttribute[]) fi.GetCustomAttributes(typeof (DisplayAttribute), false);
+
+            if (attributes.Length <= 0) return enumValue.ToString();
+            string resourceValue = _resource.GetString(attributes[0].Name);
+            return string.IsNullOrWhiteSpace(resourceValue) ? enumValue.ToString() : resourceValue;
+        }
+    }
+}
