bc7f886b5236f813616f9d6e4905f4362ec96678
Sumerano, Michael J <mjsumerano@thehealthplan.com>
2015-07-17 20:37:19 -0400

    Localized Index view - fixes #2

--- Trunc/Models/UrlItemModel.cs
+++ Trunc/Models/UrlItemModel.cs
@@ -3,21 +3,21 @@ using Trunc.Data;
 
 namespace Trunc.Models {
     public class UrlItemModel {
-        [MinLength(3, ErrorMessage = "Must be 3 or more characters")]
-        [RegularExpression(@"([a-zA-Z0-9_\-]+)", ErrorMessage="Must use only letters, numbers, - or _")]
-        [Display(Name = "Custom URL")]
+        [MinLength(3, ErrorMessageResourceName = "CustomUrlMinLength", ErrorMessageResourceType = typeof(ModelRes.UrlItemModel), ErrorMessage = null)]
+        [RegularExpression(@"([a-zA-Z0-9_\-]+)", ErrorMessageResourceName = "CustomUrlRegularExpression", ErrorMessageResourceType = typeof(ModelRes.UrlItemModel), ErrorMessage = null)]
+        [Display(Name = "CustomUrl", ResourceType = typeof(ModelRes.UrlItemModel))]
         public string CustomUrl { get; set; }
 
-        [Required]
-        [Url]
-        [Display(Name = "Origin URL")]
+        [Required(ErrorMessageResourceName = "OriginUrlRequired", ErrorMessageResourceType = typeof(ModelRes.UrlItemModel), ErrorMessage = null)]
+        [Url(ErrorMessageResourceName = "OriginUrlUrl", ErrorMessageResourceType = typeof(ModelRes.UrlItemModel), ErrorMessage = null)]
+        [Display(Name = "OriginUrl", ResourceType = typeof(ModelRes.UrlItemModel))]
         public string OriginUrl { get; set; }
 
-        [Range(1, int.MaxValue)]
-        [Display(Name = "Expire in Days")]
+        [Range(1, int.MaxValue, ErrorMessageResourceName = "ExpireInDaysRange", ErrorMessageResourceType = typeof(ModelRes.UrlItemModel), ErrorMessage = null)]
+        [Display(Name = "ExpireInDays", ResourceType = typeof(ModelRes.UrlItemModel))]
         public int ExpireInDays { get; set; }
 
-        [Display(Name = "Expire Mode")]
+        [Display(Name = "ExpireMode", ResourceType = typeof(ModelRes.UrlItemModel))]
         public ExpireMode ExpireMode { get; set; }
     }
 }
